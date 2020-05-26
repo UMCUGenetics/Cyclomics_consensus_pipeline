9b33371452396cd57f34cf39ae268349f6d81f2a
melferink <m.elferink@umcutrecht.nl>
2020-05-26 16:38:52 +0200

    python3 +refactor

--- filter_sam.py
+++ filter_sam.py
@@ -1,31 +1,32 @@
 #! /usr/bin/env python
-import sys, os,re, commands
+import os
 from optparse import OptionParser
 from optparse import OptionGroup
 import pysam
+import settings
 
 if __name__ == "__main__":
     parser = OptionParser();
     group = OptionGroup(parser, "Main options")
-    #group.add_option("-i", dest="bam", metavar="[PATH]", help=" input BAM file")
-    group.add_option("-b", dest="blacklist", help="blacklist of reads that should be excluded for basecalling")
+    group.add_option("-b", dest = "blacklist", help = "blacklist of reads that should be excluded for basecalling. File should contain 1 column with readID to exclude")
+    group.add_option("-s", default = setting.samtools, dest = "samtools", help = "full path to samtools executable [ default samtools in settings.py")
     parser.add_option_group(group)
     (opt, args) = parser.parse_args()
    
     cwd = os.getcwd()
     for f in os.listdir(cwd):
         if f.endswith((".sorted.bam")):
-            in_file=open(opt.blacklist,"r").readlines()
-            bl_dic={}
+            in_file = open(opt.blacklist,"r").readlines()
+            output_id = "{bam_id}_bl.bam".format(bam_id = str(f)[0:-4])
+            bl_dic = {}
             for line in in_file:
-                readid=line.split()[0].split("_")[0]
+                readid = line.split()[0].split("_")[0]
                 if readid not in bl_dic:
-                    bl_dic[readid]=" "
-
+                    bl_dic[readid] = " "
             bamfile = pysam.AlignmentFile(f, "rb")
-            out_bam = pysam.AlignmentFile(str(f)[0:-4]+"_bl.bam", "wb", template=bamfile)
+            out_bam = pysam.AlignmentFile(output_id, "wb", template = bamfile)
             for read in bamfile:
-                readid= read.query_name.split("_")[0]
+                readid = read.query_name.split("_")[0]
                 if readid in bl_dic:
                     pass
                 else: 
@@ -33,3 +34,7 @@ if __name__ == "__main__":
             bamfile.close()
             out_bam.close()
             os.system ("samtools index "+ str(f)[0:-4]+"_bl.bam")  
+            os.system ("{samtools} index {output_id}".format(
+                samtools =  opt.samtools,
+                output_id = output_id
+            ))
