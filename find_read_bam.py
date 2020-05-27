4b34aafc46948965fe941b15db90463a4f748615
melferink <m.elferink@umcutrecht.nl>
2020-05-27 16:04:29 +0200

    refactor

--- find_read_bam.py
+++ find_read_bam.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python
 import subprocess
 
-files= subprocess.getoutput("find ./ -iname \"*tar\"").split()
+files = subprocess.getoutput("find ./ -iname \"*tar\"").split()
 for f in files:
-    l= subprocess.getoutput("tar -tf "+str(f)).split()
+    l = subprocess.getoutput("tar -tf {f}".format(f=f)).split()
     for item in l:
         if "bai" not in item: 
-            print(f,item)
+            print(f, item)
 
 
