0c89f654b506670022a48baa9f59707f2b9a0dd1
Roy Straver <roystraver@gmail.com>
2020-07-07 17:01:11 +0200

    My local settings

--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
+import os
+
 # User specific details
 mail = 'm.elferink@umcutrecht.nl'	#Email adress
 project = 'compgen'			#Project name for Slurm cluster
 
 # Paths to scripts
-repo_path = "/hpc/compgen/tools/development/DEV_Cyclomics_consensus_pipeline/"	
-venv = str(repo_path)+ "venv/bin/activate"
+repo_path = "/Users/rstraver/Workspace/Cyclomics_consensus_pipeline/"
+venv = "/Users/rstraver/Workspace/conda_tombo/bin/activate cyclocons"
+# Don't touch, should always be the same:
 default_slurm = str(repo_path) + "run_dagcon_consensus.py"
 repeat_slurm = str(repo_path) + "bin_on_repeat_count.py"
 default_nocluster = str(repo_path) + "run_dagcon_consensus_nocluster.py"
@@ -17,26 +20,29 @@ find_read_bam = str(repo_path) + "find_read_bam.py"
 structure = str(repo_path) + "make_structure.py"
 
 # Paths to tools
-bwa = "/hpc/local/CentOS7/cog_bioinf/bwa-0.7.17/bwa"
-sambamba = "/hpc/local/CentOS7/cog/software/sambamba-0.6.5/sambamba"
-bam2m5 = "/hpc/compgen/tools/bam2m5/bam2m5.py"
-pbdagcon = "/hpc/compgen/tools/pbdagcon/src/cpp/pbdagcon"
-lastal = "/hpc/compgen/tools/last-921/src/lastal"
-lastsplit = "/hpc/compgen/tools/last-921/src/last-split"
-lastparam = str(repo_path) + "data_files/last_params"
-mafconvert = "/hpc/compgen/tools/last-921/scripts/maf-convert"
-rscript= "/hpc/local/CentOS7/common/lang/R/3.2.2/bin/Rscript"
-samtools = "/hpc/local/CentOS7/cog/software/samtools-1.2/samtools"
+tool_path = repo_path + "tools/"
+bwa = tool_path + "bwa"
+sambamba = tool_path + "sambamba_v0.6.5"
+bam2m5 = tool_path + "bam2m5-master/bam2m5.py"
+pbdagcon = tool_path + "pbdagcon"
+lastal = tool_path + "last-921/bin/lastal"
+lastsplit = tool_path + "last-921/bin/last-split"
+mafconvert = 'python2 ' + tool_path + "last-921/bin/maf-convert"
+rscript= "Rscript"
+samtools = tool_path + "samtools"
 
-# Reference Genome files 
-full_ref = "/hpc/compgen/GENOMES/Cyclomics_reference_genome/version12/Homo_sapiens.GRCh37.GATK.illumina_cyclomics_backbone.fasta"
-target_ref = "/hpc/compgen/GENOMES/Cyclomics_reference_genome/version12/BRAF_TP53_EGFR_BB_pjet.fasta"
+lastparam = repo_path + "data_files/last_params" #whut?
+cosmic = repo_path + "data_files/COSMIC_mutations.bed"
+
+# Reference Genome files
+full_ref = repo_path + "/data_files/Homo_sapiens.GRCh37.GATK.illumina_cyclomics_backbone.fasta"
+target_ref = repo_path + "/data_files/testdata/BRAF_TP53_EGFR_BB_pjet.fasta"
 
 #  General settings
-SLURM_JOB_TIME_SHORT = "2:00:00"	# Job time SLURM		
+SLURM_JOB_TIME_SHORT = "2:00:00"	# Job time SLURM
 SLURM_JOB_TIME_MED = "8:00:00"		# Job time SLURM
 SLURM_JOB_TIME_LONG = "196:00:00"	# Job time SLURM
-MAX_MEM_TARGET = 10			# Memory SLURM (low for targeted mapping) 
+MAX_MEM_TARGET = 10			# Memory SLURM (low for targeted mapping)
 MAX_MEM_FULL  = 32 			# Memory SLURM (higher for full mapping)
 THREADS = 2 				# Threads to be used in several jobs
 MIN_CONS_LEN = 35			# Minimum consensus lenght for pbdagcon
@@ -46,8 +52,8 @@ MAX_READS_JOB_PLOT = 50000  		# Maximum reads in strucure plot. Note that this n
 
 #run_dagcon_consensus.py specific settings
 DAGCON_MIN_COV = 10			# Minimum coverage needed for consensus calling with pbdagcon
-SLURM_PARALLEL_JOBS = 200		# Maximum number of parallel jobs 
-MAX_READS_JOB = 1000			# Number of reads per jobs 
+SLURM_PARALLEL_JOBS = 200		# Maximum number of parallel jobs
+MAX_READS_JOB = 1000			# Number of reads per jobs
 
 #split_forward_reverse_reads.py specific settings
 SPLIT_MIN_PERC = 50 			# Minimal percentage to determine either forward or reverse mapped insert/backbone (50% = majority vote)
@@ -63,4 +69,3 @@ STRUCTURE_OVERLAP = 100			# Overlap in bp to determine if insert fragements are 
 STRUCTURE_FLANK = 30			# Flank to determine unmapped regions to either BB or I
 STRUCTURE_MAD = 100			# If MAD score for insert startsite is more than threshold, report threshold
 STRUCTURE_INTARGET = "17:7565097-7590856"	# default repeat loci considered insert target
-
